4e830afea6ddd25339fe843481fa48349bc2128f
Ramadan-Elgamal <ramadan.reda12345@gmail.com>
2026-05-14 13:25:54 +0300

    created the starter for lab2

--- lab1/src/components/Hero.jsx
+++ lab1/src/components/Hero.jsx
@@ -3,7 +3,7 @@ import { Button } from './ui/button'
 
 const Hero = () => {
     return (
-        <div className='w-full h-[60dvh] p-12 bg-[url("/hero-bg.jpg")] bg-cover bg-center flex items-center justify-start'>
+        <div className='w-full h-[80dvh] p-12 bg-[url("/hero-bg.jpg")] bg-cover bg-center flex items-center justify-start'>
             <div className="content flex flex-col items-start justify-start h-full w-full gap-4">
                 <h1 className='text-4xl font-bold text-white'>Kaite Reed</h1>
                 <p className='text-lg text-white'>Web Developer & Designer</p>
